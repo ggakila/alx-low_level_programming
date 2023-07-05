5f0bd70706641cea9fc4511f70768ce027478e26
root <gisorehaggai@gmail.com>
2023-07-05 16:21:13 -0700

    practice

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(){
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main0(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
