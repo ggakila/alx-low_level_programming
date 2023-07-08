6e3668383de7870bb95c18ce36d428c2ac28043b
root <gisorehaggai@gmail.com>
2023-07-08 11:55:08 -0700

    clang is kinda fun

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -prints out 0-9 then a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
